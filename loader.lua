06f5f4231f7db2fce9f4e1e8e916c661e04fb492
miur1 <miureal121@gmail.com>
2026-02-06 05:57:23 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,44 @@
+local repo = "https://raw.githubusercontent.com/miureals/test/refs/heads/main/"
+local PlaceId = game.PlaceId
+
+--special games 
+local Games = {
+    [2753915549] = "bf.lua", 
+    [4442272183] = "bf.lua", 
+    [7449423635] = "bf.lua", 
+    [126884695634066] = "Grow_A_Garden.lua",
+}
+
+-- general game
+local defaultFile = "Main.lua"
+
+-- determine placeid
+local fileToLoad = Games[PlaceId] or defaultFile
+local url = repo .. fileToLoad
+
+print("[MiuHub Loader] 🧠 Game Detected:", PlaceId)
+print("[MiuHub Loader] 📂 Loading:", url)
+
+
+local success, response = pcall(function()
+    return game:HttpGet(url)
+end)
+
+if not success then
+    warn("[MiuHub Loader] ❌ failed to fetch script from URL:", response)
+    return
+end
+
+-- run script 
+local func, loadErr = loadstring(response)
+if not func then
+    warn("[MiuHub Loader] ⚠️ Gagal compile script:", loadErr)
+    return
+end
+
+local ok, runtimeErr = pcall(func)
+if not ok then
+    warn("[MiuHub Loader] 🚨script error when running:", runtimeErr)
+else
+    print("[MiuHub Loader] ✅ Script executed successfully:", fileToLoad)
+end
